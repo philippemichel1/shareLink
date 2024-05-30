f060fb2b8da15598a77de814083a06f97e252c64
Philippe MICHEL <philippe.michel1@gmail.com>
2024-05-30 19:48:30 +0200

    Initial Commit

---
+++ ShareLink/ShareLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareLinkApp.swift
+//  ShareLink
+//
+//  Created by Philippe MICHEL on 30/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShareLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
