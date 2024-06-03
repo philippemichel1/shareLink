9cae8e42a13bdc3557290e181ec8a9afc35aa721
Philippe MICHEL <philippe.michel1@gmail.com>
2024-06-03 20:07:32 +0200

    code

--- ShareLink/ContentView.swift
+++ ShareLink/ContentView.swift
@@ -8,14 +8,96 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var  image:Image?
+    @State private var inputImage:UIImage?
+    @State private var showLibrary:Bool = false
+    @State private var showView:Bool = false
+    @State private var shareText:String = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+        TabView {
+            // partager un lien
+            VStack {
+                Text("Partager un lien ")
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                    .foregroundStyle(.red)
+                    .padding()
+                
+                ShareLink(item: URL(string: "https://www.titastus.com")!) {
+                    Label("titastus.com", systemImage: "square.and.arrow.up.fill")
+                }
+            }
+            .tabItem {
+                Image(systemName: "link")
+                Text("Lien")
+            }
+            // partager du texte
+            VStack {
+                    Text("Partager du texte")
+                        .font(.system(size: 30, weight: .bold, design: .rounded))
+                        .foregroundStyle(.red)
+                        
+                    TextField("Taper votre texte", text: $shareText)
+                    .padding()
+                    .textFieldStyle(.roundedBorder)
+                    if !shareText.isEmpty {
+                        ShareLink(item: shareText) {
+                            Label("Partager", systemImage: "square.and.arrow.up.fill")
+                        }
+                }
+            }
+            .tabItem {
+                Image(systemName: "textformat.abc")
+                Text("Texte")
+            }
+            // partager une image
+            VStack {
+                Text("Partager une photo")
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                    .foregroundStyle(.red)
+                image?
+                    .resizable()
+                    .cornerRadius(10)
+                    .background(.red)
+                    .frame(width: 250, height: 250)
+                    .background(.red)
+                    .cornerRadius(5)
+                if !(image == nil) {
+                    ShareLink(item: image!, preview: SharePreview("Image", image: image!)) {
+                        Label("Partager", systemImage: "square.and.arrow.up.fill")
+                    }
+                }
+                
+                Button("Librairie Photo") {
+                    self.showLibrary.toggle()
+                }
+                .buttonStyle(PlainButtonStyle())
+                .frame(height: 50)
+                .foregroundColor(.white)
+                .padding(.horizontal)
+                .background(.blue)
+                .cornerRadius(10)
+                .padding()
+                .sheet(isPresented: $showLibrary) {
+                    PhotoPicker(image: $inputImage)
+                }
+                .onChange(of:inputImage) {loadImage()}
+            }
+            .tabItem {
+                Image(systemName: "photo")
+                Text("Photo")
+            }
+            
+            
         }
-        .padding()
+        .onAppear {
+            
+        }
+    }
+    //Chargement Image
+    func loadImage() {
+        guard let inputImage = inputImage else {return}
+        image = Image(uiImage: inputImage)
     }
 }
 
--- ShareLink/ViewModel/PhotoPicker.swift
+++ ShareLink/ViewModel/PhotoPicker.swift
@@ -5,4 +5,64 @@
 //  Created by Philippe MICHEL on 30/05/2024.
 //
 
-import Foundation
+import SwiftUI
+import PhotosUI
+
+// Définition d'une structure PhotoPicker qui se conforme au protocole UIViewControllerRepresentable
+struct PhotoPicker:UIViewControllerRepresentable {
+    @Binding var image:UIImage?
+    
+    // Définition d'une classe Coordinator pour gérer les interactions avec PHPickerViewController
+    class Coordinator:NSObject, PHPickerViewControllerDelegate {
+        var parent:PhotoPicker
+        init(parent: PhotoPicker) {
+            self.parent = parent
+        }
+        
+        // Fonction appelée lorsque la sélection d'images est terminée dans PHPickerViewController
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            
+            // Dismiss du PHPickerViewController
+            picker.dismiss(animated: true)
+            
+            // on récupère le premier résultat de la séléction
+            guard let provider = results.first?.itemProvider else {return}
+            // Vérifie si le provider peut charger un objet de type UIImage
+            if provider.canLoadObject(ofClass: UIImage.self) {
+                
+                // Charge l'objet de type UIImage à partir du provider
+                provider.loadObject(ofClass: UIImage.self) { picture, _ in
+                    //place l'image de type UIImage (image de la selection)
+                    self.parent.image = picture as? UIImage
+                }
+            }
+            
+        }
+    }
+    // Fonction pour créer une instance de PHPickerViewController
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        
+        // Configuration de PHPickerViewController pour filtrer et sélectionner uniquement des images
+        var config = PHPickerConfiguration()
+        config.filter = .images
+        
+        // Création d'une instance de PHPickerViewController avec la configuration définie
+        let picker = PHPickerViewController(configuration: config)
+        // Attribution du délégué (Coordinator) au PHPickerViewController
+        picker.delegate = context.coordinator
+        return picker
+        
+    }
+    // Fonction appelée lorsqu'une mise à jour de l'interface utilisateur est nécessaire
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
+        // pas utilisé ici mais cette fonction doit être présente pour se conformer au protocole
+        
+    }
+    
+    // Fonction pour créer une instance de Coordinator et la lier à la structure PhotoPicker
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+    }
+    
+    
+}
